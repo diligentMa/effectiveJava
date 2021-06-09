698c97d11fd394cdb57e2f61b06e72d66e2e9b3f
ma-jiajun <15513273579@163.com>
2021-06-09 21:00:22 +0800

    搭建项目

---
+++ src/main/java/com/ma/effectivejava/EffectiveJavaApplication.java
@@ -0,0 +1,16 @@
+package com.ma.effectivejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ma
+ */
+@SpringBootApplication
+public class EffectiveJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EffectiveJavaApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/ma/effectivejava/chaptertwo/StaticFactoryReplaceConstructor.java
@@ -0,0 +1,9 @@
+package com.ma.effectivejava.chaptertwo;
+
+/**
+ * @Author:MaJiaJun
+ * @Date: 2021/6/9 20:58
+ * @Description: 用静态工厂方法代替构造器
+ */
+public class StaticFactoryReplaceConstructor {
+}
